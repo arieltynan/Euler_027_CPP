5737b20df88c106e71149584bda4e5aaa14e6b72
Ariel Tynan <tynan.a@northeastern.edu>
2022-01-25 11:22:41 -0700

    Solved. Compiles in ~7 seconds

--- Euler_027_CPP.cpp
+++ Euler_027_CPP.cpp
@@ -1,20 +1,90 @@
 // Euler_027_CPP.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Quadratic Primes
+// Started 25 Jan 2022
+// Solved 25 Jan 2022
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+// binary search
+bool searchForPrime(vector<int> primes, const int target)
+{
+    int size = primes.size();
+    int first = 0;
+    int last = size - 1;
+
+    while (first <= last)
+    {
+        int mid = (first + last) / 2;
+        int midValue = primes[mid];
+
+        if (target == midValue)
+            return true;
+
+        else if (target < midValue)
+            last = mid - 1;
+
+        else
+            first = mid + 1;
+
+    } //end while
+    return false;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+    // Prime generator taken from 035
+    vector<int> primes;
+    int num = 10000; //primes less than num
+    primes.push_back(2);
+    for (int i = 3; i < num; i++)
+    {
+        bool prime = true;
+        for (int j = 0; j < primes.size() && primes[j] * primes[j] <= i; j++)
+        {
+            if (i % primes[j] == 0)
+            {
+                prime = false;
+                break;
+            }
+        }
+        if (prime)
+        {
+            primes.push_back(i);
+            //cout << i << " ";
+        }
+    } // end for loop, prime search
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+    // Iterate through combinations of a,b
+    bool isPrime = true; // checks if output is in list of primes
+    int nMax = 0, aMax = 0, bMax = 0;
+    for (int a = -1000; a < 1; a++) // iterated through 0 < a < 1000, max is 41
+    {
+        for (int b = -1000; b < 1000; b++)
+        {
+            int n = 0; // number of times n is in primes
+            while (searchForPrime(primes, n * n + a * n + b)) // while solution is prime for changing n
+                n++;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+            if (n > nMax) // sets max 
+            {
+                nMax = n;
+                aMax = a;
+                bMax = b;
+            }
+        } // end for loop b
+    } // end for loop a
+
+    // cout << "nMax: " << nMax << " aMax: " << aMax << " bMax: " << bMax << endl;
+    cout << "Answer: " << aMax * bMax;
+} // end int main
+
+// Steps
+// Create vector list of primes
+// iterate through combinations of a/b
+// Check if output is in list of primes
+// Count successful outputs
+// Send new max to max variable
+// Multiply final max positions
